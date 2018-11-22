f68081263b06cda7b1138dddb610601ab1e616b3
Bogdan <bogdan@gridconcepts.com.au>
2018-11-22 17:35:45 +0300

    Add button to admin panel in edit page mode

--- bbs-plugin.php
+++ bbs-plugin.php
@@ -5,4 +5,17 @@ Description: I have not yet invented
 Version: 1.0
 Author: Babenkobs
 */
-?>
+
+
+// Добавляет ссылку в админ бар
+add_action( 'admin_bar_menu', 'my_admin_bar_menu', 30 );
+function my_admin_bar_menu( $wp_admin_bar ) {
+    if( get_current_screen()->base == "post" ){
+        // echo "<test>". get_current_screen()->base . "</test>";
+        $wp_admin_bar->add_menu( array(
+            'id'    => 'menu_id',
+            'title' => 'ссылка',
+            'href'  => 'http://site.ru',
+        ) );
+    }
+}
